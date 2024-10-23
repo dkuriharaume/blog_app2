996a9d87854e53cedf8acd2d07c7b47524ded0cd
Daisuke Kurihara <dkurihara3352@gmail.com>
2024-10-24 07:22:56 +0900

    implement error handling on log in

--- controllers/authenticateUserController.js
+++ controllers/authenticateUserController.js
@@ -3,12 +3,28 @@ const bcrypt = require('bcrypt');
 
 module.exports = async (req, res) =>{
 
+    req.flash('data', {
+        username: req.body.username,
+        password: req.body.password
+    });
+    let isBlank = false;
+    if(!req.body.username){
+        isBlank = true;
+        req.flash('error', 'username is required.');
+    }
+    if(!req.body.password){
+        isBlank = true;
+        req.flash('error', 'password is required.');
+    }
+    if(isBlank) return res.redirect('/user/login');
+
     const users = await User.find({
         name: req.body.username
     });
 
     if(users == null || users.length < 1){
-        console.log('user with the name not found');
+        // console.log('user with the name not found');
+        req.flash('error', 'An user with the username is not found.');
         res.redirect('/user/login');
     } else {
         for(var i = 0; i < users.length; i ++){
@@ -30,7 +46,8 @@ module.exports = async (req, res) =>{
                     });
                 });
             }else{
-                console.log('password is not correct.');
+                // console.log('password is not correct.');
+                req.flash('error', 'password is not correct.');
                 return res.redirect('/user/login');
             }
         }
--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,6 +3,8 @@ module.exports = (req, res) =>{
         title: 'Login',
         pageHeaderH1: 'Login',
         pageHeaderSub: 'Welcome Back!',
-        bgImage: 'home-bg.jpg'
+        bgImage: 'home-bg.jpg',
+        error: req.flash('error'),
+        data: req.flash('data')[0]
     });
 };
