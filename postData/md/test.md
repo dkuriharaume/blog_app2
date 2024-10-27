0d702eb40f76494c7fd6ad4051f490fa82b0c7b8
Daisuke Kurihara <dkurihara3352@gmail.com>
2024-10-27 16:14:13 +0900

    add image markdown parser

---
+++ postData/md/test.md
@@ -0,0 +1,28 @@
+# This is a sample post
+
+## Does this work ?
+
+I'm trying to create a funciotionality whereby I write an MD file and it is converted into HTML and properly displayed as such on the page.
+
+If I'm to create a blog, this is a must.
+
+## Why is it so important ?
+
+Cuz I don't want to mess around with the damn visual editor stuff!
+
+### umm, why not ?
+
+I'm set up with this VS Code and Vim editor environment.  
+Everything else hinders my productivity.
+
+### image test
+
+This is image test.  
+
+![image](DSC02789.webp)
+
+### another image test
+
+And here goes another image.  
+
+![anotherImage](home-bg.jpg)
--- test.js
+++ test.js
@@ -3,7 +3,8 @@ const User = require('./models/User');
 // const BlogPost = require('./models/BlogPost');
 // const path = require('path');
 // const webp = require('webp-converter');
-// const fs = require('fs');
+const fs = require('fs');
+const md = require('marked');
 
 // webp.grant_permission();
 
@@ -13,21 +14,17 @@ async function main(){
 
     // #region Database
 
-    await mongoose.connect('mongodb://127.0.0.1:27017/blogTest2');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/blogTest2');
 
     // BlogPost.create({
     //     title: "Test Post 2",
     //     body: "This is a testing post. Does it work?"
     // });
 
-    User.create({
-        name: 'Daisuke Kurihara',
-        password: 'korneria09'
-    });
-
-
-
-
+    // User.create({
+    //     name: 'Daisuke Kurihara',
+    //     password: 'korneria09'
+    // });
     // #endregion
     
     // #region webp conversion
@@ -42,6 +39,43 @@ async function main(){
     // #region fs
     
     // fs.unlink(path.resolve(__dirname,imagePath,imageId + '.jpg'),error=>{console.log(error)});
+
+    // #endregion
+
+    // #region regex
+
+
+    // const test = '<img src="../../public/assets/img/D12345432.webp" alt="image" /> <img src="../../public/assets/img/someother.webp" alt="image" />';
+    // const re = /(?<=src=(['"]))(.*?)(?=\1)/g;
+    // const result = test.replace(re,"Hi: "+ "$2");
+    // console.log(result);
+
+    // #endregion
+
+    // #region markedown
+    
+    const input = './postData/md/test.md';
+    const output = './postData/html/test.html';
+
+    const dir = 'assets/img/';
+    const re = /(?<=src=(['"]))(.*?)(?=\1)/g;
+
+    fs.readFile(input,'utf-8',(err, data)=>{
+        if(err) console.log(err);
+
+        let html = md.parse(data);
+
+        const result = html.replace(re, dir + "$2");
+        // console.log(result);
+
+
+        fs.writeFile(output,result,(error)=>{
+            console.log(error);
+        });
+    });
+
+
+
 
     // #endregion
 }
